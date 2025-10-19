166cb275d9d1a9ec53e6e94aa5d7d6ac57b1022a
youssef634 <youssef.khaled8666@gmail.com>
2025-10-19 16:44:17 +0300

    init

---
+++ src/main.ts
@@ -0,0 +1,43 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as bodyParser from 'body-parser';
+import * as path from 'path';
+import { join } from 'path';
+
+async function bootstrap() {
+  dotenv.config();
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {});
+
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+  app.useStaticAssets(path.join(__dirname, '..', 'public'), {
+    prefix: '/api/public',
+  });
+
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads',
+  });
+
+  app.setGlobalPrefix('/api');
+
+  const PORT = process.env.PORT || 3001;
+
+  await app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+  });
+}
+
+bootstrap();
