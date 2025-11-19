1a2c274cc6b6ff18d17da2be91958dd91c26d25d
Abd-elrahmann <cds.abdelrahmanmohamed74057@alexu.edu.eg>
2025-11-19 22:19:15 +0200

    merge

---
+++ prisma/migrations/20251118221912_revert_partner_profit_percent/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `partnerProfitPercent` on the `Partner` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Partner" DROP COLUMN "partnerProfitPercent";
