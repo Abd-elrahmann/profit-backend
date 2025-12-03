076e053d64d443b94323fc400246e62f7983979d
youssef634 <youssef.khaled8666@gmail.com>
2025-12-03 14:47:15 +0200

    user active token

--- dist/src/auth/strategy/jwt.strategy.d.ts
+++ dist/src/auth/strategy/jwt.strategy.d.ts
@@ -1,11 +1,13 @@
 import { Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
-declare const JwtStrategy_base: new (...args: [opt: import("passport-jwt").StrategyOptionsWithRequest] | [opt: import("passport-jwt").StrategyOptionsWithoutRequest]) => Strategy & {
+import { PrismaService } from '../../prisma/prisma.service';
+declare const JwtStrategy_base: new (...args: [opt: import("passport-jwt").StrategyOptionsWithoutRequest] | [opt: import("passport-jwt").StrategyOptionsWithRequest]) => Strategy & {
     validate(...args: any[]): unknown;
 };
 export declare class JwtStrategy extends JwtStrategy_base {
     private readonly configService;
-    constructor(configService: ConfigService);
+    private prisma;
+    constructor(configService: ConfigService, prisma: PrismaService);
     validate(payload: any): Promise<{
         id: any;
         email: any;
--- dist/src/auth/strategy/jwt.strategy.js
+++ dist/src/auth/strategy/jwt.strategy.js
@@ -14,23 +14,31 @@ const common_1 = require("@nestjs/common");
 const passport_1 = require("@nestjs/passport");
 const passport_jwt_1 = require("passport-jwt");
 const config_1 = require("@nestjs/config");
+const prisma_service_1 = require("../../prisma/prisma.service");
 let JwtStrategy = class JwtStrategy extends (0, passport_1.PassportStrategy)(passport_jwt_1.Strategy, 'jwt') {
     configService;
-    constructor(configService) {
+    prisma;
+    constructor(configService, prisma) {
         super({
             jwtFromRequest: passport_jwt_1.ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
             secretOrKey: configService.get('JWT_SECRET') ?? 'defaultsecret',
         });
         this.configService = configService;
+        this.prisma = prisma;
     }
     async validate(payload) {
+        const user = await this.prisma.user.findUnique({ where: { id: payload.sub } });
+        if (!user || !user.isActive) {
+            throw new common_1.UnauthorizedException('User logged out');
+        }
         return { id: payload.sub, email: payload.email };
     }
 };
 exports.JwtStrategy = JwtStrategy;
 exports.JwtStrategy = JwtStrategy = __decorate([
     (0, common_1.Injectable)(),
-    __metadata("design:paramtypes", [config_1.ConfigService])
+    __metadata("design:paramtypes", [config_1.ConfigService,
+        prisma_service_1.PrismaService])
 ], JwtStrategy);
 //# sourceMappingURL=jwt.strategy.js.map
