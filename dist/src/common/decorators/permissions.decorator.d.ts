b3166b376c28909f5b7eb595467c2545698802e5
Abd-elrahmann <cds.abdelrahmanmohamed74057@alexu.edu.eg>
2025-12-19 16:03:52 +0200

    Remove dist folder from repository

--- dist/src/common/decorators/permissions.decorator.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const PERMISSION_KEY = "permission";
-export declare const Permissions: (module: string, action: "canView" | "canAdd" | "canUpdate" | "canDelete" | "canPost") => import("@nestjs/common").CustomDecorator<string>;
--- dist/src/notification/dto/notification.dto.d.ts
+++
@@ -1,8 +0,0 @@
-import { TemplateType } from "@prisma/client";
-export declare class SendNotificationDto {
-    templateType: TemplateType;
-    clientId: number;
-    loanId?: number;
-    repaymentId?: number;
-    channel?: string;
-}
