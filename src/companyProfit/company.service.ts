325ffcfc43e94ab3528b3d289ad55be78a068690
youssef634 <youssef.khaled8666@gmail.com>
2026-01-25 21:36:01 +0200

    logs2

--- src/companyProfit/company.service.ts
+++ src/companyProfit/company.service.ts
@@ -193,7 +193,9 @@ export class CompanyService {
                 const netUnrounded = gross - expenseShare;
                 const netRounded = Math.floor(netUnrounded);
                 centsFromPartners += netUnrounded - netRounded;
-                console.log({ partnerId, gross, expenseShare, netUnrounded, netRounded, centsFromPartners });
+                console.log(
+                    `[PARTNER ${partnerId}] gross=${gross} expense=${expenseShare.toFixed(4)} net=${netUnrounded.toFixed(4)} rounded=${netRounded} cents=${centsFromPartners.toFixed(4)}`
+                );
             }
             centsFromPartners = Number(centsFromPartners.toFixed(2));
 
